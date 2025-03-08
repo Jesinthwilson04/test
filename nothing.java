2ba79a689400bfb444516a76c88a9d0c2b526b8d
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-03-08 14:23:01 +0530

    changes

--- nothing.java
+++ nothing.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class nothing {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String inp = sc.nextLine();
-        printfun(inp);
+        
+        printfun("hello world");
 
     }
 
