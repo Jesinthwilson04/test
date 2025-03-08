2771ce055add77aabb1de198abce8ea5058ad92d
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-03-08 14:22:27 +0530

    chenc

--- nothing.java
+++ nothing.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class nothing {
 
     public static void main(String[] args) {
-
-        printfun("hello world");
+        Scanner sc = new Scanner(System.in);
+        String inp = sc.nextLine();
+        printfun(inp);
 
     }
 
     public static void printfun(String inp) {
         System.out.println(inp);
-        // this is the change that i done in practice branch
+        // this is the practice branch
     }
 }
